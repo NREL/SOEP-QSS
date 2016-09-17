9c0b699b47f51bbfa1a8ea556fea5b1ee23bfe64
Stuart Mentzer <Stuart_Mentzer@objexx.com>
2016-09-17 00:48:35 -0400

    VariableQSS2 unit test added / VariableQSS1 unit test updated

--- tst/QSS/unit/VariableQSS2.unit.cc
+++ tst/QSS/unit/VariableQSS2.unit.cc
@@ -1,41 +1,44 @@
-// QSS::VariableQSS1 Unit Tests
+// QSS::VariableQSS2 Unit Tests
 
 // Google Test Headers
 #include <gtest/gtest.h>
 
 // QSS Headers
-#include <QSS/VariableQSS1.hh>
+#include <QSS/VariableQSS2.hh>
 
 // C++ Headers
 #include <algorithm>
+#include <cmath>
 
-TEST( VariableQSS1Test, Basic )
+TEST( VariableQSS2Test, Basic )
 {
-	VariableQSS1 x1( "x1" );
+	VariableQSS2 x1( "x1" );
 	x1.init_val( 2.5 );
 	x1.add_der( 12.0 ).add_der( 2.0, x1 );
 	x1.init_der();
+	x1.init_der2();
 	x1.init_event();
 	EXPECT_EQ( 1.0e-6, x1.aTol );
 	EXPECT_EQ( 1.0e-6, x1.rTol );
-	EXPECT_EQ( 2.5, x1.q( 1.0e-7 ) );
-	EXPECT_EQ( 0.0, x1.q1( 1.0e-7 ) );
-	EXPECT_DOUBLE_EQ( 2.5 + 17.0e-7, x1.x( 1.0e-7 ) );
+	EXPECT_DOUBLE_EQ( 2.5 + 17.0e-6, x1.q( 1.0e-6 ) );
+	EXPECT_EQ( 17.0, x1.q1( 1.0e-6 ) );
+	EXPECT_DOUBLE_EQ( 2.5 + 17.0e-6 + 17.0E-12, x1.x( 1.0e-6 ) );
 	EXPECT_EQ( 0.0, x1.tBeg );
-	EXPECT_DOUBLE_EQ( std::max( x1.aTol, x1.rTol * 2.5 ) / 17.0, x1.tEnd );
+	EXPECT_DOUBLE_EQ( std::sqrt( std::max( x1.aTol, x1.rTol * 2.5 ) / 17.0 ), x1.tEnd );
 	double const x1_tEnd( x1.tEnd );
 	x1.advance();
 	EXPECT_EQ( x1_tEnd, x1.tBeg );
 
-	VariableQSS1 x2( "x2", 1.0e-3, 1.0e-4 );
+	VariableQSS2 x2( "x2", 1.0e-3, 1.0e-4 );
 	x2.init_val( 2.5 );
 	x2.add_der( 12.0 ).add_der( 2.0, x2 );
 	x2.init_der();
+	x2.init_der2();
 	x2.init_event();
 	EXPECT_EQ( 1.0e-3, x2.aTol );
 	EXPECT_EQ( 1.0e-4, x2.rTol );
 	EXPECT_EQ( 0.0, x2.tBeg );
-	EXPECT_DOUBLE_EQ( std::max( x2.aTol, x2.rTol * 2.5 ) / 17.0, x2.tEnd );
+	EXPECT_DOUBLE_EQ( std::sqrt( std::max( x2.aTol, x2.rTol * 2.5 ) / 17.0 ), x2.tEnd );
 
 	EXPECT_EQ( 2U, events.size() );
 	events.clear();
