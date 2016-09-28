d7f99752b8eeac0dc615098a886501cc45216207
Stuart Mentzer <Stuart_Mentzer@objexx.com>
2016-09-27 22:08:51 -0400

    Master controller refinements

--- src/QSS/QSS.cc
+++ src/QSS/QSS.cc
@@ -27,10 +27,15 @@ main()
 
 	enum QSS_Method { QSS1 = 1, QSS2, QSS3, QSS4 };
 
+	// Settings
+	bool const sampled( false ); // Sampled outputs?
+	bool const all_vars_out( false ); // Output all variables at every requantization event?
+	double const dto( 1.0e-3 ); // Sampling time step
+
 //	// Simple x, y, z
-//	QSS_Method const qss_max( QSS2 );
 //	double const tEnd( 5.0 );
-//	double t( 0.0 ), dto( 1.0e-3 ), to( dto );
+//	double t( 0.0 ), to( t + dto );
+//	QSS_Method const qss_max( QSS2 );
 //	VariableQSS2 x( "x", 1.0, 0.0 );
 //	VariableQSS2 y( "y", 1.0, 0.0 );
 //	VariableQSS2 z( "z", 1.0, 0.0 );
@@ -51,9 +56,9 @@ main()
 //	// A      = [[0, -1], [+1, 0]]
 //	// Q      = 1
 //	// order  = 1
-//	QSS_Method const qss_max( QSS2 );
 //	double const tEnd( 10.0 );
-//	double t( 0.0 ), dto( 1.0e-3 ), to( dto );
+//	double t( 0.0 ), to( t + dto );
+//	QSS_Method const qss_max( QSS2 );
 //	VariableQSS2 x1( "x1", 1.0, 0.0 );
 //	VariableQSS2 x2( "x2", 1.0, 0.0 );
 //	x1.init_val( 1.0 );
@@ -65,9 +70,9 @@ main()
 //	vars.push_back( &x2 );
 
 	// Achilles and the Tortoise
-	QSS_Method const qss_max( QSS2 );
 	double const tEnd( 10.0 );
-	double t( 0.0 ), dto( 1.0e-3 ), to( dto );
+	double t( 0.0 ), to( t + dto );
+	QSS_Method const qss_max( QSS2 );
 	VariableQSS2 x1( "x1", 1.0, 0.0 );
 	VariableQSS2 x2( "x2", 1.0, 0.0 );
 	x1.init_val( 0.0 );
@@ -102,14 +107,16 @@ main()
 		x_streams.back() << std::setprecision( 16 ) << t << '\t' << var->x( t ) << '\n';
 	}
 	size_type n_vars( vars.size() );
-	while ( ( t <= tEnd ) || ( to <= tEnd ) ) {
+	while ( ( t <= tEnd ) || ( sampled && ( to <= tEnd ) ) ) {
 		t = events.top_time();
-		while ( to < std::min( t, tEnd ) ) { // Sampled outputs
-			for ( size_type i = 0; i < n_vars; ++i ) { // Comment this block out for no sampled output
-				q_streams[ i ] << to << '\t' << vars[ i ]->q( to ) << '\n';
-				x_streams[ i ] << to << '\t' << vars[ i ]->x( to ) << '\n';
+		if ( sampled ) { // Sampled outputs
+			while ( to < std::min( t, tEnd ) ) {
+				for ( size_type i = 0; i < n_vars; ++i ) {
+					q_streams[ i ] << to << '\t' << vars[ i ]->q( to ) << '\n';
+					x_streams[ i ] << to << '\t' << vars[ i ]->x( to ) << '\n';
+				}
+				to += dto;
 			}
-			to += dto;
 		}
 		if ( events.simultaneous() ) {
 //			std::cout << "Simultaneous trigger event at t = " << t << std::endl;
@@ -136,11 +143,18 @@ main()
 			}
 			for ( Variable * trigger : triggers ) {
 				if ( t <= tEnd ) { // Requantization outputs
-					for ( size_type i = 0; i < n_vars; ++i ) { // Variable needs access to its stream to avoid this loop
-						if ( trigger == vars[ i ] ) {
+					if ( all_vars_out ) {
+						for ( size_type i = 0; i < n_vars; ++i ) {
 							q_streams[ i ] << t << '\t' << vars[ i ]->q( t ) << '\n';
 							x_streams[ i ] << t << '\t' << vars[ i ]->x( t ) << '\n';
-							break;
+						}
+					} else {
+						for ( size_type i = 0; i < n_vars; ++i ) { // Give Variable access to its stream to avoid this loop
+							if ( trigger == vars[ i ] ) {
+								q_streams[ i ] << t << '\t' << vars[ i ]->q( t ) << '\n';
+								x_streams[ i ] << t << '\t' << vars[ i ]->x( t ) << '\n';
+								break;
+							}
 						}
 					}
 				}
@@ -150,11 +164,18 @@ main()
 			assert( trigger->tEnd == t );
 			trigger->advance();
 			if ( t <= tEnd ) { // Requantization outputs
-				for ( size_type i = 0; i < n_vars; ++i ) { // Variable needs access to its stream to avoid this loop
-					if ( trigger == vars[ i ] ) {
+				if ( all_vars_out ) {
+					for ( size_type i = 0; i < n_vars; ++i ) {
 						q_streams[ i ] << t << '\t' << vars[ i ]->q( t ) << '\n';
 						x_streams[ i ] << t << '\t' << vars[ i ]->x( t ) << '\n';
-						break;
+					}
+				} else {
+					for ( size_type i = 0; i < n_vars; ++i ) { // Give Variable access to its stream to avoid this loop
+						if ( trigger == vars[ i ] ) {
+							q_streams[ i ] << t << '\t' << vars[ i ]->q( t ) << '\n';
+							x_streams[ i ] << t << '\t' << vars[ i ]->x( t ) << '\n';
+							break;
+						}
 					}
 				}
 			}
