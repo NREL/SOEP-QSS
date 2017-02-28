52f8d20a8c9848c5f2d92a52350acfb46acd1420
Stuart Mentzer <Stuart_Mentzer@objexx.com>
2017-02-28 01:00:43 -0500

    setGTest.bat fixups

--- bin/Windows/GCC/64/setGTest.bat
+++ bin/Windows/GCC/64/setGTest.bat
@@ -4,4 +4,4 @@ rem Google Test GCC 64-Bit Setup
 rem Put a custom version first in your PATH to adapt to your system
 
 set GTEST_INC=C:\gtest\include
-set GTEST_BIN=C:\gtest\bin\MinGW\64
+set GTEST_BIN=C:\gtest\bin\GCC\64\dll
