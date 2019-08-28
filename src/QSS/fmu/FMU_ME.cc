2045732e0c71c18a2446d6c8164a45de1d785d08
Stuart Mentzer <Stuart_Mentzer@objexx.com>
2019-08-28 01:04:44 -0400

    Elim duplicate observer outputs at requantizations

--- src/QSS/fmu/FMU_ME.cc
+++ src/QSS/fmu/FMU_ME.cc
@@ -2018,13 +2018,6 @@ namespace fmu {
 						if ( doROut ) { // Requantization output: after requantization
 							size_type const i( var_idx[ trigger ] );
 							if ( options::output::q ) q_outs[ i ].append( t, trigger->q( t ) );
-							for ( Variable const * observer : trigger->observers() ) { // Observer output
-								if ( observer->is_ZC() ) { // Zero-crossing variables requantize in observer advance
-									size_type const io( var_idx[ observer ] );
-									if ( options::output::x ) x_outs[ io ].append( t, observer->x( t ) );
-									if ( options::output::q ) q_outs[ io ].append( t, observer->q( t ) );
-								}
-							}
 						}
 					} else { // Simultaneous triggers
 						++n_QSS_simultaneous_events;
@@ -2081,13 +2074,6 @@ namespace fmu {
 							for ( Variable const * trigger : triggers ) { // Triggers
 								size_type const i( var_idx[ trigger ] );
 								if ( options::output::q ) q_outs[ i ].append( t, trigger->q( t ) );
-							}
-							for ( Variable const * observer : observers_s ) { // Observer output
-								if ( observer->is_ZC() ) { // Zero-crossing variables requantize in observer advance
-									size_type const io( var_idx[ observer ] );
-									if ( options::output::x ) x_outs[ io ].append( t, observer->x( t ) );
-									if ( options::output::q ) q_outs[ io ].append( t, observer->q( t ) );
-								}
 							}
 						}
 					}
