6a1cb6b97857ded452ae0ed01db5cb3cce0af9ab
Stuart Mentzer <Stuart_Mentzer@objexx.com>
2019-08-29 03:18:15 -0400

    Build refinements

--- bin/Windows/VC/64/d/GNUmakeinit.mk
+++ bin/Windows/VC/64/d/GNUmakeinit.mk
@@ -7,7 +7,6 @@
 # Variables
 CXXFLAGS := /nologo /TP /EHsc /W4 /wd4068 /wd4996 /DNOMINMAX /DWIN32_LEAN_AND_MEAN /DWIN32 /DQSS_FMU /Od /Ob0 /Z7 /RTCsu /MDd
 CXXFLAGS += /wd4100 /wd4505 # For FMIL headers
-CFLAGS := /nologo /TC /W3 /wd4068 /wd4244 /wd4996 /DWIN32_LEAN_AND_MEAN /DWIN32 /Od /Ob0 /Z7 /RTCsu /MDd
 # Disabled Warnings:
 #  4068 Unknown pragma
 #  4100 Unreferenced formal parameter
@@ -15,7 +14,7 @@ CFLAGS := /nologo /TC /W3 /wd4068 /wd4244 /wd4996 /DWIN32_LEAN_AND_MEAN /DWIN32 
 #  4996 Deprecated functions (/D_SCL_SECURE_NO_WARNINGS /D_CRT_SECURE_NO_WARNINGS /D_CRT_NONSTDC_NO_WARNINGS)
 # Notes:
 #  /D_CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES=1 /D_CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES_COUNT=1 enables secure template overloads
-LDFLAGS := /nologo /F8388608
-LINKFLAGS := /link /ignore:4099 /LIBPATH:$(QSS_bin) /DEBUG
+CXXLINKFLAGS := /nologo
+LINKFLAGS := /link /nologo /SUBSYSTEM:CONSOLE /STACK:8388608 /DEBUG /IGNORE:4099
 
 include $(QSS_bin)\..\GNUmakeinit.mk
--- bin/Windows/VC/64/p/GNUmakeinit.mk
+++ bin/Windows/VC/64/p/GNUmakeinit.mk
@@ -7,7 +7,6 @@
 # Variables
 CXXFLAGS := /nologo /TP /EHsc /W4 /wd4068 /wd4996 /DNOMINMAX /DWIN32_LEAN_AND_MEAN /DWIN32 /DQSS_FMU /DNDEBUG /arch:AVX2 /openmp /O2 /fp:fast /Qpar /GS- /Z7 /MD
 CXXFLAGS += /wd4100 /wd4505 # For FMIL headers
-CFLAGS := /nologo /TC /W3 /wd4068 /wd4244 /wd4996 /DWIN32_LEAN_AND_MEAN /DWIN32 /DNDEBUG /arch:AVX2 /openmp /O2 /fp:fast /GS- /Z7 /MD
 # Disabled Warnings:
 #  4068 Unknown pragma
 #  4100 Unreferenced formal parameter
@@ -15,7 +14,7 @@ CFLAGS := /nologo /TC /W3 /wd4068 /wd4244 /wd4996 /DWIN32_LEAN_AND_MEAN /DWIN32 
 #  4996 Deprecated functions (/D_SCL_SECURE_NO_WARNINGS /D_CRT_SECURE_NO_WARNINGS /D_CRT_NONSTDC_NO_WARNINGS)
 # Notes:
 #  /D_CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES=1 /D_CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES_COUNT=1 enables secure template overloads
-LDFLAGS := /nologo /F8388608 /DEBUG /PROFILE
-LINKFLAGS := /link /LIBPATH:$(QSS_bin) /DEBUG /PROFILE
+CXXLINKFLAGS := /nologo
+LINKFLAGS := /link /nologo /SUBSYSTEM:CONSOLE /STACK:8388608 /DEBUG /PROFILE
 
 include $(QSS_bin)\..\GNUmakeinit.mk
