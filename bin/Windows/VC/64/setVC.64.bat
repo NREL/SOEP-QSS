9e41b6a6fd1dabe5ff39bb98624bb4bebc44d591
Stuart Mentzer <Stuart_Mentzer@objexx.com>
2019-12-22 17:14:16 -0500

    Intel C++ 2020 and Visual C++ 2019 support

--- bin/Windows/VC/64/setVC.64.bat
+++ bin/Windows/VC/64/setVC.64.bat
@@ -1,21 +1,18 @@
 @echo off
 rem Visual C++ Compiler 64-Bit Setup
 
-if exist "%ProgramFiles32%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" (
-  "%ProgramFiles32%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+if exist "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+  "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 ) else (
-if exist "%ProgramFiles32%\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
-  "%ProgramFiles32%\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
+if exist "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+  "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 ) else (
-if exist "%ProgramFiles32%\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" (
-  "%ProgramFiles32%\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+if exist "%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
+  "%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 ) else (
-if exist "%ProgramFiles32%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
-  "%ProgramFiles32%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-) else (
-if exist "%ProgramFiles32%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
-  "%ProgramFiles32%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+if exist "%PROGRAMFILES(X86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+  "%PROGRAMFILES(X86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
 ) else (
   echo Visual C++ not found
   exit /B 1
-)))))
+))))
