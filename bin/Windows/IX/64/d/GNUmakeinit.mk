c195530dba7d604e04f15a999a158194d730409d
Stuart Mentzer <Stuart_Mentzer@objexx.com>
2023-03-22 10:58:25 -0400

    Use VC gtest libs for IC/IX / Makefile refinements

--- bin/Windows/IX/64/d/GNUmakeinit.mk
+++ bin/Windows/IX/64/d/GNUmakeinit.mk
@@ -6,6 +6,6 @@
 
 # Variables
 CXXFLAGS := /nologo /Qstd=c++20 /Qansi-alias /Qiopenmp /QxHOST /EHsc /Wall /DNOMINMAX /DWIN32_LEAN_AND_MEAN /D_CRT_SECURE_NO_WARNINGS /D_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING /DWIN32 /Od /debug /debug:inline-debug-info /fp:strict /Gs0 /GS /traceback /Zi /MDd
-LINKFLAGS := /link /nologo /SUBSYSTEM:CONSOLE /LIBPATH:$(QSS_bin) /STACK:16777216 /DEBUG
+LINKFLAGS := /link /nologo /SUBSYSTEM:CONSOLE /LIBPATH:$(QSS_bin) /STACK:16777216 /DEBUG /IGNORE:4099
 
 include $(QSS_bin)\..\GNUmakeinit.mk
--- bin/Windows/IX/64/setGTest.bat
+++ bin/Windows/IX/64/setGTest.bat
@@ -4,5 +4,5 @@
 :: Put a custom version first in your PATH to adapt to your system
 
 set GTEST_INC=C:\gtest\include
-set GTEST_BIN=C:\gtest\bin\IX\64\MDd
+set GTEST_BIN=C:\gtest\bin\VC\64\MDd
 set Path=%Path%;%GTEST_BIN%
