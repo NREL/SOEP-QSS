d094d3beac3c39f87edc3c1c0535191cf51dbede
Stuart Mentzer <Stuart_Mentzer@objexx.com>
2023-04-20 17:10:06 -0400

    Rename setProject scripts to setQSS

--- bin/Windows/GCC/setQSS.bat
+++ bin/Windows/GCC/setQSS.bat
@@ -7,9 +7,9 @@ set QSS=%QSS:~0,-17%
 :Step2
 
 set PlatformOS=Windows
-set PlatformCompiler=Windows\GCC
+set PlatformOSCompiler=Windows\GCC
 
-set Path=%Path%;%QSS%\bin\%PlatformCompiler%
+set Path=%Path%;%QSS%\bin\%PlatformOSCompiler%
 
 set CPATH=.;%QSS%\src
 if not (%FMIL_HOME%) == () set CPATH=%CPATH%;%FMIL_HOME%\include;%FMIL_HOME%\include\FMI;%FMIL_HOME%\include\FMI1;%FMIL_HOME%\include\FMI2;;%FMIL_HOME%\include\JM
